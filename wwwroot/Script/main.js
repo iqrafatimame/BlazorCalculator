0a17e630d97ce92fe230285ad54d500f4e88d64a
iqrafatimame <iqrafatimame@gmail.com>
2021-12-10 16:26:28 +0500

    ⚡️ Improved the performance and handled some errors

--- wwwroot/Script/main.js
+++ wwwroot/Script/main.js
@@ -1,22 +1,31 @@
+let isEvaluated = false
 
 function addElement(Value){
     const screen = document.getElementById('screen')
-    screen.value += Value;
+    if(isEvaluated){
+        screen.value = ''
+        isEvaluated = false
+    }
+    screen.value += Value  
 }
 
 function clearScreen(){
     const screen = document.getElementById('screen')
-    screen.value = '';
+    screen.value = ''
 }
 
 function Evaluate(){
     const screen = document.getElementById('screen')
+    console.log("flag: ", isEvaluated)
+    
     try{
         if(!screen.value.length == 0){
             screen.value = eval(screen.value)
+            isEvaluated = true
         }
     }
     catch{
         screen.value = 'Error!'
     }
+
 }
